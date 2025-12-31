57352d1f25ddb9311b1d542c2f6945616b512635
gaotianming <tianminggao1@gmail.com>
2025-12-31 14:36:59 +0800

    Initial commit

---
+++ src/main/java/com/highskyguy1/highoreblocks/HighsOresAndBlocksDataGenerator.java
@@ -0,0 +1,11 @@
+package com.highskyguy1.highoreblocks;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class HighsOresAndBlocksDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
---
+++ src/main/java/com/highskyguy1/highoreblocks/ModClient.java
@@ -0,0 +1,10 @@
+package com.highskyguy1.highoreblocks;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
